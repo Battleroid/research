017d3c953d1a740fc12c797ed92c6f8acea041a1
Battleroid <casweed@gmail.com>
2015-02-09 08:55:16 -0500

    gets largest eigenvalue & corresponding set of eigenvectors, will regen matrix if ki/j has 0

--- rm.py
+++ rm.py
@@ -1,13 +1,22 @@
 import numpy as np
+from numpy.linalg import linalg
+
 
 class RM:
     def __init__(self, size=10):
         self.size = size
         self.a = self._create_symm_matrix(self._create_matrix())
         self.ki, self.kj, self.m = self._create_ki_kj_m(self.a)
+        # to assure that a random array is created that has at least 1 in every cell
+        while 0 in self.ki:
+            self.a = self._create_symm_matrix(self._create_matrix())
+            self.ki, self.kj, self.m = self._create_ki_kj_m(self.a)
         self.p1 = self._create_p1()
         self.p = self._create_p()
         self.b = self.a - self.p
+        self.eval, self.evec = linalg.eig(self.b)
+        self.max_eig_val = self._get_largest_eig_val(self.eval)
+        self.max_eig_val_vec = self.evec[self.max_eig_val]
 
     def _create_matrix(self):
         """Creates random matrix with values (0, 1) based on size.
@@ -60,3 +69,13 @@ class RM:
                 except ZeroDivisionError:
                     pass
         return a
+
+    @staticmethod
+    def _get_largest_eig_val(matrix):
+        """
+        Gets the largest index of largest eigenvalue.
+
+        :param matrix:
+        :return integer:
+        """
+        return matrix.argmax()
